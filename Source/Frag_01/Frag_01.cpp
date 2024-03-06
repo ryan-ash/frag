77c04d4d62d4be004d0feac46338a1989a3550fc
Ryan Ash <scarletspark@gmail.com>
2024-03-06 02:18:27 +0100

    C++ project conversion

---
+++ Source/Frag_01/Frag_01.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Frag_01.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Frag_01, "Frag_01" );
